e673880fd000f1235df2569dd6f8382283b80621
Thomas Menzel <menzelths@yahoo.de>
2015-01-08 00:35:29 +0100

    neu formatiert

--- OrientDBTest3/src/main/java/de/qreator/orientdbtest3/OrientDBTester.java
+++ OrientDBTest3/src/main/java/de/qreator/orientdbtest3/OrientDBTester.java
@@ -50,7 +50,7 @@ public class OrientDBTester {
         OGlobalConfiguration.WAL_MAX_SIZE.setValue(1024); // größe in mb,wie groß der gesamte wal-speicher sein darf
 
         boolean existiertSchon = true;
-        String dbPath = "plocal:/Users/thomas/dbOrient/db";
+        String dbPath = "plocal:/Users/thomas/dbOrient/db2";
 
         db = new ODatabaseDocumentTx(dbPath);
         if (!db.exists()) {
@@ -68,11 +68,11 @@ public class OrientDBTester {
             for (int j = 5; j <= 10; j++) {
                 tag += (char) ((int) (65 + Math.random() * 24));
             }
-            //System.out.println(tag);
+            
             tags[i] = tag;
         }
 
-        //OrientGraph graph = new OrientGraph("plocal:/Users/thomas/dbOrient/db");    
+          
         try {
             OrientGraphFactory factory = new OrientGraphFactory(dbPath).setupPool(1, 10);
             graph = factory.getTx();
@@ -134,8 +134,6 @@ public class OrientDBTester {
 
                     Vertex doc = graph.addVertex("class:doc");
                     doc.setProperty("docname", tag);
-
-                    // System.out.print("Dokument "+tag+" mit id "+doc.getId().toString()+": ");
                     int anzahl = (int) (Math.random() * (maxTags - minTags + 1) + minTags);
 
                     for (int j = 0; j < anzahl; j++) {
@@ -143,35 +141,14 @@ public class OrientDBTester {
                         Vertex v = al.get(zz);
                         doc.addEdge("hatTag", v);
                         v.setProperty("anzahl", (Long) (v.getProperty("anzahl")) + 1);
-                    //graph.addEdge("class:hatTag", doc, al.get(zz),null);
-
-                        //  System.out.print(" "+al.get(zz).getProperty("tagname"));
                     }
-
-                    // System.out.println("");
                     graph.commit();
                 } catch (Exception e) {
                     graph.rollback();
                 }
             }
         }
-            //Iterator<Vertex> it2 = graph.getVertices("tagname", tags[3]).iterator();
-        //System.out.println("Hole Vertex 3 mit Inhalt " + tags[3] + ":" + it2.next().getProperty("tagname"));
-        /* GremlinPipeline pipe=new GremlinPipeline();
-         pipe.start(graph.getVertex("#9:3")).property("tagname");
-         int treffer=0;
-         while (pipe.hasNext()){
-         treffer++;
-            
-         System.out.println("Treffer "+treffer+": "+pipe.next());
-         }*/
-        /* Vertex luca = graph.addVertex(null); // 1st OPERATION: IMPLICITLY BEGIN A TRANSACTION
-         luca.setProperty("name", "Thomas");
-         Vertex marko = graph.addVertex(null);
-         marko.setProperty("name", "Patrick");
-         Edge lucaKnowsMarko = graph.addEdge(null, luca, marko, "knows");
-         graph.commit();*/
-        // tests zur geschwindigkeit
+           
         boolean weiter = true;
         HashMap m = new HashMap();
         Iterator<Vertex> it9 = graph.getVerticesOfClass("tags").iterator(); //haupttag holen   
@@ -182,28 +159,17 @@ public class OrientDBTester {
         ArrayList<Vertex> svT = new ArrayList<>();
         GremlinPipeline pipe2 = new GremlinPipeline();
         Vertex startVertex = it9.next();
-        pipe2.start(startVertex).out("hat");//.in("hatTag").dedup(); // mit altem tag starten
-        //pipe2.out("hatTag").groupCount(m).dedup();
+        pipe2.start(startVertex).out("hat");
 
         Iterator<Vertex> it10 = pipe2.iterator();
         while (it10.hasNext()) {
             // legt die hashmap an
             Vertex v = it10.next();
             m.put(v, v.getProperty("anzahl"));
-            //long anzahlEcken=(new GremlinPipeline(v)).inE("hatTag").count();
-            //m.put(v, graph.getEdges);
             svT.add(v);
-
         }
 
-        /* pipe2 = new GremlinPipeline();
-         pipe2.start(startVertex).out("hat").inE("hatTag").count();
-         it10 = pipe2.iterator();
-         while (it10.hasNext()) {
-         // legt die hashmap an
-         //svT.add(it10.next());
-         System.out.println(it10.next());
-         }*/
+        
         long zeit2 = (new Date()).getTime();
         System.out.println("Dauer für Tagsuche: " + (zeit2 - zeit1) + " ms");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -213,7 +179,6 @@ public class OrientDBTester {
         HashMap docmap = new HashMap<>();
         boolean neustart = false;
         Table t = new Table();
-        //Pipe p=Gremlin.compile("_().table(t){it.name}");
         while (weiter) {
             // liste aller sinnvollen tags ausgeben
             System.out.println("Liste der Tags: ");
@@ -228,9 +193,7 @@ public class OrientDBTester {
                     }
                     System.out.println("");
 
-                } /*else {
-                 System.out.println((i + 1) + ": " + svT.get(i).getProperty("tagname"));
-                 }*/
+                } 
 
             }
             if (t.getRowCount() > 0) {
@@ -274,8 +237,6 @@ public class OrientDBTester {
                     GremlinPipeline pipe = new GremlinPipeline();
                     if (i > 0) {
                         i--;
-                        //auswahl.add(graph.getVertex(svT.get(i).getId()));
-                        //pipe.start(graph.getVertex(svT.get(i).getId())).in("hatTag").dedup();
                         auswahl.add(svT.get(i));
                         pipe.start(svT.get(i)).in("hatTag").dedup();
                         svT.remove(i);
@@ -289,8 +250,7 @@ public class OrientDBTester {
                         if (auswahl.size() > 0) {
                             pipe.start(auswahl.get(0)).in("hatTag").dedup(); // mit altem tag starten
                         } else {
-                            //Iterator<Vertex> it8 = graph.getVerticesOfClass("tags").iterator(); //haupttag holen                       
-                            pipe.start(startVertex).out("hat"); // mit altem tag starten   
+                            pipe.start(startVertex).out("hat"); // mit starttag starten   
                             neustart = true;
                         }
 
@@ -298,8 +258,7 @@ public class OrientDBTester {
 
                     m = new HashMap();
                     t = new Table();
-                    /* docs=new ArrayList<>();
-                     docmap=new HashMap();*/
+                    
 
                     for (int j = 0; j < auswahl.size(); j++) { // überprüft alle tags (entspricht "und")
                         pipe.as("x").out("hatTag").has("tagname", auswahl.get(j).getProperty("tagname")).back("x");
@@ -314,13 +273,7 @@ public class OrientDBTester {
 
                     while (it6.hasNext()) {
                         treffer++;
-
                         Vertex v = it6.next();
-                        /*   if (docs.size()>0){
-                         docmap.put(v, docs);
-                         docs=new ArrayList<>();
-                         }*/
-
                         if (neustart) {
                             m.put(v, v.getProperty("anzahl"));
                         }
@@ -328,18 +281,6 @@ public class OrientDBTester {
                     }
                     neustart = false;
 
-                    //svT.clear();
-                 /*   for (int k = 0; k < temp.size(); k++) {
-                     svT.add(temp.get(k));
-                     }*/
-                    // map mit anzahlen ausgeben
-                   /* Iterator<Vertex> i7 = m.keySet().iterator();
-                     while (i7.hasNext()) {
-                     Vertex v = i7.next();
-                     System.out.print("Tag: " + v.getProperty("tagname"));
-                     System.out.println(", Anzahl: " + m.get(v));
-
-                     }*/
                     zeit2 = (new Date()).getTime();
                     System.out.println("Treffer: " + treffer + ", Dauer: " + (zeit2 - zeit1) + " ms");
                     letzteZeit = (zeit2 - zeit1);
